11f64d6bc18f26c547834cde9e4711514d56e930
Gunty <guntyad@gmail.com>
2025-04-19 23:52:16 -0600

    First Commit
    Last Updated: 03/17/2025

---
+++ main.py
@@ -0,0 +1,42 @@
+# import colorgram
+#
+# rgb = []
+# colors = colorgram.extract('image.jpg', 30)
+#
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r,g,b)
+#     rgb.append(new_color)
+#
+# print(rgb)
+import random
+import turtle as tutel
+
+tutel.colormode(255)
+vedal = tutel.Turtle()
+vedal.speed("fastest")
+color_list = [(253, 251, 247), (253, 248, 252), (235, 252, 243), (198, 13, 32), (248, 236, 25), (40, 76, 188), (244, 247, 253), (39, 216, 69), (238, 227, 5), (227, 159, 49), (29, 40, 154), (212, 76, 15), (17, 153, 17), (241, 36, 161), (195, 16, 12), (223, 21, 120), (68, 10, 31), (61, 15, 8), (223, 141, 206), (11, 97, 62), (219, 159, 11), (54, 209, 229), (19, 21, 49), (238, 157, 216), (79, 74, 212), (10, 228, 238), (73, 212, 168), (93, 233, 198), (65, 231, 239), (217, 88, 51)]
+
+vedal.hideturtle()
+vedal.penup()
+vedal.setheading(225)
+vedal.forward(300)
+vedal.setheading(0)
+
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots):
+    vedal.dot(20, random.choice(color_list))
+    vedal.forward(50)
+
+    if dot_count % 10 == 0:
+        vedal.setheading(90)
+        vedal.forward(50)
+        vedal.setheading(180)
+        vedal.forward(500)
+        vedal.setheading(0)
+
+screen = tutel.Screen()
+screen.exitonclick()
